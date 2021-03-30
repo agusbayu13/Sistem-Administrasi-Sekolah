dc23f0141fb3d425d2dd4f647712f11a589f2405
Rohmatul Laeli Arba'an Nisa <80687802+arbaan11@users.noreply.github.com>
2021-03-30 21:11:03 +0700

    update login php
    menambah perintah pemrosesan form login

--- login.php
+++ login.php
@@ -22,5 +22,32 @@
 			</tr>
 		</table>
 	</form>
+<?php
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		//variabel untuk menyimpan kiriman dari form
+		$user = $_POST['username'];
+		$pass = $_POST['password'];
+
+		if($user=='' || $pass==''){
+			echo "Form belum lengkap!";
+		}else{
+			include "koneksi.php";	
+			$sqlLogin = mysqli_query($konek, "SELECT * FROM admin WHERE username='$user' AND password='$pass'");
+			$jml = mysqli_num_rows($sqlLogin);
+			$d=mysqli_fetch_array($sqlLogin);
+			if($jml > 0){
+				session_start();
+				$_SESSION['login']		= true;
+				$_SESSION['id']			= $d['idadmin'];
+				$_SESSION['username']	= $d['username'];
+
+				header('location:./index.php');
+			}else{
+				echo "Username dan Password anda salah!";
+			}
+		}
+
+	}
+	?>
 </body>
 </html>
