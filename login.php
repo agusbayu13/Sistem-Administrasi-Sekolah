b8ceda1b63e008b02c3c5c84b97959e953ed269e
agusbayu13 <abay@students.unnes.ac.id>
2021-03-30 09:53:45 +0700

    Halaman login sistem pertama

--- login.php
+++ login.php
@@ -2,16 +2,25 @@
 <html>
 <head>
 	<title>Login</title>
-
-	
 </head>
 <body>
-
-<?php
-
-
-?>
-
-</script>
+	<h3>Silahkan Login </h3>
+	<hr/>
+	<form method="post" action="">
+		<table>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="username"/></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password"/></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="login"/></td>
+			</tr>
+		</table>
+	</form>
 </body>
 </html>
