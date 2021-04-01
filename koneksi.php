433d839722b094b5d5554134720708fe80905806
Agus Bayu Pamungkas <80688503+agusbayu13@users.noreply.github.com>
2021-04-02 04:27:24 +0700

    Update koneksi.php
    Coba database lagi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,9 @@
 <?php
-//variabel koneksi
-
-$konek = mysqli_connect ("localhost", "root", "", "database-github"); //databasespp nanti diganti nama database nya
-
+$host="178.128.20.122";
+$username="root";
+$password="Multisystem19SI";
+$namadatabase="database-github";
+$connection=mysql_connect($host, $username, $password) or die("Kesalahan Koneksi...!!");
+mysql_select_db($namadatabase, $connection) or die("Database Gagal terkoneksi");
+echo “Koneksi Sukses”;
 ?>
