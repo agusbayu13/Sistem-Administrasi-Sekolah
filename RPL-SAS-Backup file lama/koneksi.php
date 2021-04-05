6623ebde296bad044cbe7418e82edae4614a4639
agusbayu13 <abay@students.unnes.ac.id>
2021-04-06 05:29:18 +0700

    update user

--- RPL-SAS-Backup file lama/koneksi.php
+++ RPL-SAS-Backup file lama/koneksi.php
@@ -1,9 +1,4 @@
 <?php
-$host="178.128.20.122";
-$username="root";
-$password="Multisystem19SI";
-$namadatabase="database-github";
-$connection=mysql_connect($host, $username, $password) or die("Kesalahan Koneksi...!!");
-mysql_select_db($namadatabase, $connection) or die("Database Gagal terkoneksi");
-echo “Koneksi Sukses”;
-?>
+//variabel koneksi
+
+$konek = mysqli_connect("localhost", "root", "", "database-github"); //databasespp nanti diganti nama database nya
