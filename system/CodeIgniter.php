e80596c7bd4b68dc86229eb8a741c3d04b54690c
Agus Bayu Pamungkas <80688503+agusbayu13@users.noreply.github.com>
2021-04-05 07:51:47 +0700

    back to default

--- system/CodeIgniter.php
+++ system/CodeIgniter.php
@@ -221,7 +221,7 @@ class CodeIgniter
 
 		if ($missingExtensions)
 		{
-			throw FrameworkException::forMissingExtension(\CodeIgniter\Exceptions\$extension;
+			throw FrameworkException::forMissingExtension(implode(', ', $missingExtensions));
 		}
 	}
 
