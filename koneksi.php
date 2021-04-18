de6a287e5bd3c92ccc34df675a53b3d8d1ae5572
cnisak012@students.unnes.ac.id <cnisak012@students.unnes.ac.id>
2021-04-18 23:00:30 +0700

    numpang abis ini dihapus

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,19 @@
 <?php
-$host="178.128.20.122";
-$username="root";
-$password="Multisystem19SI";
-$namadatabase="database-github";
-$connection=mysql_connect($host, $username, $password) or die("Kesalahan Koneksi...!!");
-mysql_select_db($namadatabase, $connection) or die("Database Gagal terkoneksi");
-echo “Koneksi Sukses”;
-?>
+ 	// Membuka Session
+	session_start();
+	// Menset jam dan tanggal secara default dengan lokasi asia jakarta
+	date_default_timezone_set("Asia/Jakarta");
+
+	$host		= "178.128.20.122";
+	$username	= "root";
+	$password	= "";
+	$database	= "sistem_administrasi_sekolah";  
+	// membuat variable koneksi
+	$koneksi = mysqli_connect($host, $username, $password, $database);
+	// cek koneksi berhasil atau tidak
+	if ($koneksi) {
+		//echo "berhasil terkoneksi";
+	} else {
+		echo "gagal";
+	}
+ ?>
