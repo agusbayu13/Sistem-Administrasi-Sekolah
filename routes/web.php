94a08ae9a882ebe14c8924d66463ba7c037dc4ea
Ariqah01 <ariqahsalma@students.unnes.ac.id>
2021-04-25 14:08:07 +0700

    Tambah routes login, register, dan logout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,30 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+
+Route::get('/', [AuthController::class, 'showFormlogin'])->name('login');
+Route::get('login', [AuthController::class, 'showFormlogin'])->name('login');
+Route::post('login', [AuthController::class, 'login']);
+Route::get('register', [AuthController::class, 'showFormregister'])->name('register');
+Route::post('register', [AuthController::class, 'register']);
+
+Route::group(['middleware' => 'auth'], function () {
+    
+    Route::get('home', [HomeController::class, 'index'])->name('home');
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 });
+
 
 Route::get('/transaksi', function () {
     return view('transaksi');
 });
 
 Route::get('/about', function () {
-    return view('About');
+    return view('about');
 });
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::get('/forgot-password', function () {
-    return view('forgot-password');
-});
-
+    
 Route::get('/tagihan', function () {
     return view('tagihan');
 });
@@ -44,7 +43,7 @@ Route::get('/tagihan', function () {
 Route::get('/presensi', function () {
     return view('presensi');
 });
-
+    
 Route::get('/data_guru', function () {
     return view('data_guru');
 });
