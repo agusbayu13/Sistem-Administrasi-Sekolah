16d227c9f0cab4c3efad571a5bf4aff25bb08fd5
samuelsuliadi <80687347+samuelsuliadi@users.noreply.github.com>
2021-04-27 02:11:05 +0700

    update container in data_guru dan presensi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Selamat Datang <strong>{{ Auth::user()->name }}</strong></h1>
-
+    	
 </div>
 <!-- /.container-fluid -->
 @include('sweetalert::alert')
