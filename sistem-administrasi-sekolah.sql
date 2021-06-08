c6277db1d108afcee83f7cff153d3558c6d80239
chairunnisak <80693958+chairunnisak-png@users.noreply.github.com>
2021-06-08 23:23:17 +0700

    Update sistem-administrasi-sekolah.sql

--- sistem-administrasi-sekolah.sql
+++ sistem-administrasi-sekolah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 31, 2021 at 11:33 AM
+-- Generation Time: Jun 08, 2021 at 06:02 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
@@ -44,7 +44,7 @@ CREATE TABLE `failed_jobs` (
 --
 
 CREATE TABLE `guru` (
-  `nama` varchar(100) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
   `nip` int(20) NOT NULL,
   `jeniskelamin` enum('L','P') NOT NULL,
   `alamat` varchar(100) NOT NULL,
@@ -59,7 +59,7 @@ CREATE TABLE `guru` (
 --
 
 CREATE TABLE `karyawan` (
-  `nama` varchar(100) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
   `nip` int(20) NOT NULL,
   `jeniskelamin` enum('L','P') NOT NULL,
   `alamat` varchar(100) NOT NULL,
@@ -107,16 +107,17 @@ CREATE TABLE `password_resets` (
 --
 
 CREATE TABLE `pembayaran` (
-  `id_admin` int(15) NOT NULL,
-  `NIS` int(15) NOT NULL,
-  `nama` varchar(20) NOT NULL,
+  `id` int(15) NOT NULL,
+  `nis` int(15) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
   `kelas` varchar(10) NOT NULL,
-  `waktu_pembayaran` date NOT NULL,
-  `kode_pembayaran` varchar(10) NOT NULL,
-  `total_pembayaran` varchar(20) NOT NULL,
-  `jenis_pembayaran` varchar(10) NOT NULL,
+  `tgl_pembayaran` date NOT NULL,
+  `kode_pembayaran` varchar(100) NOT NULL,
+  `total_pembayaran` varchar(100) NOT NULL,
+  `jenis_pembayaran` varchar(100) NOT NULL,
+  `bukti_pembayaran` varchar(100) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` int(11) DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -145,7 +146,7 @@ CREATE TABLE `presensi` (
 CREATE TABLE `siswa` (
   `nis` int(20) NOT NULL,
   `id_kelas` int(20) NOT NULL,
-  `nama` varchar(100) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
   `jeniskelamin` enum('L','P') NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -160,7 +161,8 @@ CREATE TABLE `siswa` (
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `level` enum('Admin','Guru','Karyawan','Siswa') COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -193,6 +195,12 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `presensi`
 --
 ALTER TABLE `presensi`
@@ -220,6 +228,12 @@ ALTER TABLE `failed_jobs`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
